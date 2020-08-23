42f080f1dfb2d4daf0928da51f046e78cd84d1c4
萩原涼 <hagiwararyo@hagihararyounoMacBook-Pro.local>
2020-08-23 16:15:48 +0900

    wordpressのインストール

--- hello/recipes/default.rb
+++ hello/recipes/default.rb
@@ -17,3 +17,22 @@ end
 package "php56-mysqlnd" do
     action :install
 end
+
+remote_file '/root/latest-ja.tar.gz' do
+    source 'http://ja.wordpress.org/latest-ja.tar.gz'
+    owner 'root'
+    group 'root'
+    mode '0444'
+    action :create
+  end
+  
+  script "install_wordpress" do
+    interpreter "bash"
+    user        "root"
+    cwd         "/root"
+    code <<-EOL
+      tar xzvf latest-ja.tar.gz
+      mv wordpress /usr/share/nginx/html 
+    EOL
+    action :run
+  end
